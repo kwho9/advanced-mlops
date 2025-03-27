b101c27b9b2fbd16959724e0adba618b73eece3c
kwho9 <157669834+kwho9@users.noreply.github.com>
2025-03-27 10:11:46 +0900

    new

--- pipelines/continuous_training/continuous_training_dag.py
+++ pipelines/continuous_training/continuous_training_dag.py
@@ -47,7 +47,19 @@ with DAG(
 
     )
 
-    data_preprocessing = EmptyOperator(task_id="data_preprocessing")
+    #data_preprocessing = EmptyOperator(task_id="data_preprocessing")
+    data_preprocessing = BashOperator(
+        task_id="data_preprocessing",
+        bash_command=f"cd {airflow_dags_path}/piplines/continuous_traning/docker &&"
+        "docker compose up --build && docker compose down",
+        env={
+            "PYTHON_FILE": "/home/codespace/data_preprocessing/preprocessor.py", 
+            "MODEL_NAME": "credit_score_classification",
+            "BASE_DT": "{{ ds }}"
+        },
+        append_env=True,
+        retries=1,
+    )
 
     training = EmptyOperator(task_id="model_training")
 
